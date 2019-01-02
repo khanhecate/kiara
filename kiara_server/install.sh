bab6d8434efdc2df0e4de5dd9c211fdffb73a209
kHaNaFi <35915223+khanhecate@users.noreply.github.com>
2019-01-02 22:32:07 +0700

    Update install.sh

--- kiara_server/install.sh
+++ kiara_server/install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo 'Installing Kiara Server ...'
 echo 'Creating Directory for Kiara server ...'
-mkdir /kiara 
+mkdir /opt/kiara 
 status=$?
 if [[ $status = 0 ]]; then
 	echo 'Copying file to Kiara folder ....'
-	cp -r -v db kiara_server.sh tmp /kiara
+	cp -r -v db kiara_server.sh tmp /opt/kiara
 	echo 'Adding Kiara to /usr/bin ...'
-	echo 'Moving to Directory /kiara ...'
-	cd /kiara
+	echo 'Moving to Directory /opt/kiara ...'
+	cd /opt/kiara
 	touch kiara
 	echo "#!/bin/bash" > kiara
 	echo "echo 'Moving to kiara Directory ...'" >> kiara
-	echo "cd /kiara" >> kiara
+	echo "cd /opt/kiara" >> kiara
 	echo "./kiara_server.sh" >> kiara
 	chmod 755 kiara 
 	mv kiara /usr/bin
@@ -20,12 +20,12 @@ if [[ $status = 0 ]]; then
 	useradd kiara
 	echo "password for user kiara"
 	passwd kiara
-	echo '(active) : kiara' > /kiara/db/db.user
+	echo '(active) : kiara' > /opt/kiara/db/db.user
 	echo "Kiara need Mysql server ! for web Interface (comming soon)"
 	echo "Kiara Installing Success !"
 	echo 'You can run kiara , by type "kiara" '
 	echo 'thanks for Installing kiara :D'
 else echo 'Updating Kiara ...'
-	cp -r -v kiara_server.sh tmp /kiara
+	cp -r -v kiara_server.sh tmp /opt/kiara
 fi
 echo "done"
