1ef8f7ac3b2113e35dc8e675ef0201fdff0ea6d8
kHaNaFi <35915223+khanhecate@users.noreply.github.com>
2019-01-03 11:27:17 +0700

    Update os.sh

--- kiara_server/config/os.sh
+++ kiara_server/config/os.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /
 grep DISTRIB_DESCRIPTION /etc/*-release > /tmp/session.txt
-sed -i 's/\/etc\/lsb-release:DISTRIB_DESCRIPTION=//g' tmp/session.txt
+grep PRETTY_NAME= /etc/*-release > /tmp/session.txt
+sed -i 's/DISTRIB_DESCRIPTION=//g' tmp/session.txt
+sed -i 's/PRETTY_NAME=//g' tmp/session.txt
+sed -i 's/"//g' tmp/session.txt
 cat /tmp/session.txt
